dd61a27ad253c9040f553990a3a884cfaaa4847c
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-02-21 00:20:11 +0100

    sql > c++

--- sql/db_schema_new.sql
+++ sql/db_schema_new.sql
@@ -85,7 +85,7 @@ create table tbl_user
 		on delete cascade
 )$
 
-# Is base of: tbl_thread, tbl_post
+# Is base of: tbl_content_thread, tbl_content_post
 # Derives from:
 create table tbl_content_base
 (
@@ -457,6 +457,60 @@ create table tbl_group_section_permission
 
 
 
+create procedure mk_content_base
+(
+	in v_id_author int,
+	out v_created_id int
+)
+begin
+	insert into tbl_content_base
+		(id_author, creation_timestamp)
+		values(v_id_author, now());
+
+	set v_created_id := LAST_INSERT_ID();
+end$
+
+create procedure mk_content_thread
+(
+	in v_id_author int,
+	in v_id_section int,
+	in v_title varchar(255)
+)
+begin
+	call mk_content_base(v_id_author, @out_id_base);
+
+	insert into tbl_content_thread
+		(id_base, id_section, title)
+		values(@out_id_base, v_id_section, v_title);
+end$
+
+create procedure mk_content_post
+(
+	in v_id_author int,
+	in v_id_thread int,
+	in v_contents text
+)
+begin
+	call mk_content_base(v_id_author, @out_id_base);
+
+	insert into tbl_content_post
+		(id_base, id_thread, contents)
+		values(@out_id_base, v_id_thread, v_contents);
+end$
+
+create procedure mk_content_attachment
+(
+	in v_id_author int,
+	in v_id_post int,
+	in v_id_file_data int
+)
+begin
+	call mk_content_base(v_id_author, @out_id_base);
+
+	insert into tbl_content_attachment
+		(id_base, id_post, id_file_data)
+		values(@out_id_base, v_id_post, v_id_file_data);
+end$
 
 
 
@@ -626,7 +680,7 @@ begin
 	# Get useful variables
 	select id, id_thread
 	into @last_post_id, @last_post_thread
-	from tbl_post
+	from tbl_content_post
 	order by id desc limit 1;
 
 	label_loop:
