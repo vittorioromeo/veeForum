4e67f4237cdbf2d6c7b84d8c9df6668e44a4c819
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-05-23 18:59:58 +0200

    prog

--- latex/mkThesis.sh
+++ latex/mkThesis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 latexpp ./thesis.lpp > ./thesis.tex
-pdflatex -shell-escape ./thesis.tex && chromium ./thesis.pdf
+pdflatex -shell-escape ./thesis.tex && pdflatex -shell-escape ./thesis.tex && chromium ./thesis.pdf
